412c5c5e79fb9dee423e200ac9eeabd3967c5a48
Stanislau <willstesi@gmail.com>
2024-07-14 15:50:44 +0300

    feat: add register sw and connect root layout

--- src/app/entry-point.tsx
+++ src/app/entry-point.tsx
@@ -1,20 +1,33 @@
+import "@/shared/styles/globals.css"
+
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 import { Route, Routes } from "react-router-dom"
+import { registerSW } from "virtual:pwa-register"
 
+import RootLayout from "./layouts/root-layout.tsx"
 import { Provider } from "./provider.tsx"
 
-import "./globals.css"
 import IndexPage from "@/pages/index.tsx"
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("У вас старая версия приложения. Обновить?")) {
+      updateSW(true)
+    }
+  },
+})
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider>
-        <Routes>
-          <Route element={<IndexPage />} path="/" />
-        </Routes>
+        <RootLayout>
+          <Routes>
+            <Route element={<IndexPage />} path="/" />
+          </Routes>
+        </RootLayout>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>,
